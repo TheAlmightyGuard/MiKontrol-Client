8301f5e3a45683c2a987a713d2852c2a134a4a8e
Ryann Pastolero <ryannapastolero@gmail.com>
2026-02-12 17:12:02 -0600

    Testing OAuth2 Handling

--- api/routes.py
+++ api/routes.py
@@ -1,9 +1,22 @@
 # FastAPI
 from discord.ext import commands
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
-# FastAPI
+# Serialization
+from itsdangerous import URLSafeSerializer
+import secrets
+import os, httpx
+
+from rich.console import Console
+
+console = Console()
+
+# Variables
 router = APIRouter()
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+serializer = URLSafeSerializer(secret_key=os.getenv("OAUTH2_SECRET"), salt="oauth2-dashboard")
 
 def set_client(discord_client : commands.Bot):
     global client
@@ -122,3 +135,70 @@ async def getGuildData(user_id : str):
         return response
     except Exception as e:
         return {"First throw": "ahh"}
+
+
+# Below is reimagined version:
+
+@router.get("/auth/login")
+async def login():
+    url = f"https://discord.com/oauth2/authorize?client_id=1463680415012491337&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fauth%2Fcallback&scope=guilds+identify+guilds.members.read"
+    return RedirectResponse(url)
+
+@router.get("/auth/callback")
+async def login_callback(code: str):
+    console.log(code)
+    
+    async with httpx.AsyncClient() as client:
+        token_payload = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': 'http://localhost:8000/auth/callback',
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        release = await client.post(url='https://discord.com/api/oauth2/token', data=token_payload, headers=headers)
+
+        if not release.is_success:
+            return { "Error" : "Failed to retrieve!"}
+        release = release.json()
+
+        authorization_type = release["token_type"]
+        authorization_token = release["access_token"]
+        expiration = release["expires_in"]
+
+        user_header = {
+            "Authorization": f"{authorization_type} {authorization_token}"
+        }
+
+        user_data = await client.get("https://discord.com/api/v10/users/@me", headers=user_header)
+        user_data = user_data.json()
+
+        session_payload = {
+            'id': user_data['id'],
+            'username': user_data['username'],
+            'global_name': user_data['global_name'],
+            'avatar': user_data['avatar'],
+        }
+
+        signed = serializer.dumps(session_payload)
+
+        response = RedirectResponse("https://mikontrol.ca/dashboard")
+
+        response.set_cookie(
+            "session",
+            signed,
+            60*60*24*7,
+            httponly=False,
+            secure=True,
+            samesite="lax"
+        )
+
+        return response
+
+        
+
+
+
